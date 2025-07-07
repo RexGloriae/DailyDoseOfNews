4bcc9c93e1389c6146f8b5041bc9ab2265e252ad
Kessel <andreidanielcodreanu@gmail.com>
2025-07-07 12:13:19 +0300

    add AI support

---
+++ src/llm.py
@@ -0,0 +1,24 @@
+from hidden.key import API_KEY
+from google import genai
+from google.genai import types
+from google.genai.errors import ClientError
+
+client = genai.Client(api_key=API_KEY)
+
+def get_description(link):
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=f"Da-mi o descriere de o propozitie a continutului: {link}",
+            config=types.GenerateContentConfig(
+            thinking_config=types.ThinkingConfig(thinking_budget=0) # Disables thinking
+            ),
+        )
+        return response.text
+    except ClientError as e:
+        if "RESOURCE_EXHAUSTED" in str(e):
+            print(f"[Quota exceeded] Skipping article: {link}")
+            return None
+        else:
+            print(f"[ClientError] Unexpected error: {e}")
+            return None
