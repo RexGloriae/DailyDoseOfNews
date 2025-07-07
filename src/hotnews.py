8307261485662643ad7f31bc33b5b8e4f10993b3
Kessel <andreidanielcodreanu@gmail.com>
2025-07-07 10:30:48 +0300

    rewrite hotnews into a class

--- src/hotnews.py
+++ src/hotnews.py
@@ -1,53 +1,51 @@
-import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
-from urllib.parse import urljoin
+from parser import download_site
 
-BASE_URL = "https://hotnews.ro/ultima-ora"
-SITE = "HotNews"
+class HotNews:
+    def __init__(self):
+        self.URL = "https://hotnews.ro/ultima-ora"
+        self.SITE = "HotNews"
+    
+    def load_list(self):
+        self.html = download_site(self.URL)
 
-def download_list_page():
-    response = requests.get(BASE_URL, headers={"User-Agent": "Mozilla/5.0"})
-    response.raise_for_status()
-    return response.text
+    def parse_articles(self):
+        soup = BeautifulSoup(self.html, "html.parser")
+        results = []
+        today = datetime.now().date()
 
-def parse_articles(html):
-    soup = BeautifulSoup(html, "html.parser")
-    results = []
-    today = datetime.now().date()
+        articles = soup.select("article.ultima-ora")
 
-    articles = soup.select("article.ultima-ora")
+        for article in articles:
+            time_tag = article.select_one("time.entry-date")
+            if not time_tag:
+                continue
 
-    for article in articles:
-        time_tag = article.select_one("time.entry-date")
-        if not time_tag:
-            continue
+            published_iso = time_tag.get("datetime")
+            if not published_iso:
+                continue
 
-        published_iso = time_tag.get("datetime")
-        if not published_iso:
-            continue
+            published_dt = datetime.fromisoformat(published_iso.replace("Z", "+00:00"))
+            if published_dt.date() != today:
+                continue
 
-        published_dt = datetime.fromisoformat(published_iso.replace("Z", "+00:00"))
-        if published_dt.date() != today:
-            continue
+            title_tag = article.select_one("h2.entry-title a")
+            title = title_tag.get_text(strip=True) if title_tag else None
+            link = title_tag.get("href") if title_tag else None
 
-        title_tag = article.select_one("h2.entry-title a")
-        title = title_tag.get_text(strip=True) if title_tag else None
-        link = title_tag.get("href") if title_tag else None
+            cat_tag = article.select_one(".hn-category-tag a")
+            category = cat_tag.get_text(strip=True) if cat_tag else None
 
-        cat_tag = article.select_one(".hn-category-tag a")
-        category = cat_tag.get_text(strip=True) if cat_tag else None
+            excerpt_tag = article.select_one("a.excerpt")
+            content = excerpt_tag.get_text(strip=True) if excerpt_tag else None
 
-        excerpt_tag = article.select_one("a.excerpt")
-        description = excerpt_tag.get_text(strip=True) if excerpt_tag else None
+            results.append({
+                "source": "HotNews",
+                "title": title,
+                "url": link,
+                "published_at": published_dt.strftime("%d/%m/%y %H:%M"),
+                "content": content
+            })
 
-        results.append({
-            "source": "HotNews",
-            "title": title,
-            "url": link,
-            "category": category,
-            "published_at": published_dt.strftime("%d/%m/%y %H:%M"),
-            "description": description
-        })
-
-    return results
+        return results
